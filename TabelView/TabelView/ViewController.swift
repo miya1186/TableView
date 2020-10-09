6a9a87d9ddb8d4a7c67efc9a0bace2ece2cd3a18
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-10-09 13:27:30 +0900

    first commit

---
+++ TabelView/TabelView/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  TabelView
+//
+//  Created by miyazawaryohei on 2020/10/09.
+//
+
+import UIKit
+
+let sectionTitle = ["チョウ目","バッタ目","コウチュウ目"]
+let section0 = [("キタテハ","タテハチョウ科"),("クロアゲハ","アゲハチョウ科")]
+let section1 = [("キリギリス","キリギリス科"),("ヒナバッタ","バッタ科"),("マツムシ","マツムシ科")]
+let section2 = [("ハンミョウ","ハンミョウ科"),("アオオサムシ","オサムシ科"),("チビクワガタ","クワガタムシ科")]
+let tableData = [section0,section1,section2]
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let myTabelView = UITableView(frame: view.frame, style: .grouped)
+        myTabelView.delegate = self
+        myTabelView.dataSource = self
+        view.addSubview(myTabelView)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionData = tableData[section]
+        return sectionData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let sectionData = tableData[(indexPath as NSIndexPath).section]
+        let cellData = sectionData[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = cellData.0
+        cell.detailTextLabel?.text = cellData.1
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let title = sectionTitle[indexPath.section]
+        let sectionData = tableData[indexPath.section]
+        let cellData = sectionData[indexPath.row]
+        print("\(title)\(cellData.1)")
+        print("\(cellData.0)")
+    }
+}
+
